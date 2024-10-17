ac19209b1c5fb2bea0058a67fe959e5441af915f
Nikita <nyuskov@gmail.com>
2024-10-17 17:46:17 +0300

    Add models

--- messages_saver/mails/models.py
+++ messages_saver/mails/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class MailUser(models.Model):
+    email = models.CharField(max_length=254)
+    login = models.CharField(max_length=120)
+    password = models.CharField(max_length=50)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(MailUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    sended_at = models.DateTimeField()
+    received_at = models.DateTimeField()
+
+
+class Attachment(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    data = models.FileField()

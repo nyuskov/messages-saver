0aa363c4c121b88c0110e1e86080866d1267cecf
Nikita <nyuskov@gmail.com>
2024-10-17 14:14:04 +0300

    Add postgresql

--- messages_saver/messages_saver/settings.py
+++ messages_saver/messages_saver/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'messages_saver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'messages_saver',
+        'USER': 'postgres',
+        'PASSWORD': 'Xx123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
